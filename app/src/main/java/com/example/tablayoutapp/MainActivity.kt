96a8f0b9d3ebe0d40295c33b7e86ec00bc67c3e9
Yukio <elsie.atieno@student.moringaschool.com>
2024-02-22 10:22:32 +0300

    TabAdapeter Mediator

--- app/src/main/java/com/example/tablayoutapp/MainActivity.kt
+++ app/src/main/java/com/example/tablayoutapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.tablayoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.example.tablayoutapp.Adapter.ViewAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    val tabsArray = arrayOf("Books", "Games", "Movies")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewpager = findViewById<ViewPager2>(R.id.viewpager)
+        val tablayout = findViewById<TabLayout>(R.id.tablayout)
+
+        val adapter = ViewAdapter(
+            supportFragmentManager, lifecycle
+        )
+
+        viewpager.adapter = adapter
+        TabLayoutMediator(tablayout, viewpager){
+            tab, position -> tab.text = tabsArray[position]
+        }.attach()
     }
 }
